6362c2495b9695a3ead101258bf57966905403f9
amitkas <amit.kashtan@gmail.com>
2017-07-09 13:50:13 +0300

    calss1

--- public/app.js
+++ public/app.js
@@ -1,5 +1,39 @@
 var app = angular.module('scotcha', ['ui.router']);
 
-app.config(function($stateProvider, $urlRouterProvider) {
+app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: 'partial-home.html'
+    })
+    .state('home.list', {
+      url: '/list',
+      templateUrl: 'partial-home-list.html',
+      controller: 'HomeListCtrl'
+    })
+        .state('home.about', {
+      url: '/about',
+      templateUrl: 'partial-home-about.html'
+    })
+      .state('scotch', {
+    url: '/scotch/:type?temp',
+    templateUrl: 'partial-scotch.html',
+    controller: function($scope, $stateParams) {
+      $scope.myScotch = $stateParams;
+    }
+  })
+    .state('scotchlibrary', {
+    url: '/library/:type',
+    templateUrl: function ($stateParams){
+      console.log($stateParams)
+      return 'partial-scotch-' + $stateParams.type + '.html';
+    }
+  })
+  
+  $locationProvider.html5Mode(true);
 
-}); 
+});
+
+app.controller('HomeListCtrl', function($scope) {
+  $scope.scotches = ['Balvenie', 'The Balmore', 'Highland Park'];
+})
--- server.js
+++ server.js
@@ -2,6 +2,11 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
+app.use(express.static('node_modules'));
+
+app.all('*', function(req, res) {
+  res.sendFile(__dirname + "/public/index.html")
+})
 
 app.listen(8000, function () {
   console.log("yo yo yo, on 8000!!")
